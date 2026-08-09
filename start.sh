a102b33b401b59b4f8eb36159d64ef135e1a517f
Inam Ur Rehman <inamurrehman634@gmail.com>
2026-08-09 00:18:06 -0700

    Added start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,12 @@
+#!/bin/bash
+set -e
+
+STREAMLIT_PORT="${PORT:-8501}"
+
 uvicorn app:app --host 0.0.0.0 --port 8000 &
+
+sleep 3
+
 streamlit run streamlit_app.py --server.port "$STREAMLIT_PORT" --server.address 0.0.0.0 &
+
 wait
